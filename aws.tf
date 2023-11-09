74e266bf9b4686db83a5cdef5ad98a332e0533ea
codesclutter <96595431+codesclutter@users.noreply.github.com>
2023-11-09 15:51:53 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+    access_key = "abc"
+    secret_key = "xyz"
+    region = "us-east-1"
+  
+}
 
+
+resource "aws_instance" "os1" {
+    ami = "ami-05a5f6298acdb05b6"
+    instance_type = "t2.micro"
+  
+}
